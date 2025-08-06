245cdc92479b479b126bda3883b3b963948b6829
0xWhosyourdaddy <zuozheng@tomo.inc>
2025-08-06 12:44:43 +0800

    add base config

--- script/ts/config.ts
+++ script/ts/config.ts
@@ -26,4 +26,14 @@ export const networkConfig: networkConfigInfo = {
         walletCoreAddress: "0x6b483BE98D5fcA84d51Dff932EC1b87A9921F756",
         storageImpl: "0x4bE37E398bB78CBD003c06724f1820aaDA59E6dB",
     },
+
+    base: {
+        rpc: process.env.BASE_RPC_URL!,
+        usdtAddress: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+        usdcAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+        tomoSwapAddress: "0x7947e5f78E78190eE2d62E36F04Ca008C7b69Afd",
+        permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+        walletCoreAddress: "0x6b483BE98D5fcA84d51Dff932EC1b87A9921F756",
+        storageImpl: "0x4bE37E398bB78CBD003c06724f1820aaDA59E6dB",
+    },
 };
